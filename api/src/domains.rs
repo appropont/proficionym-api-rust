ebc1b35555c51c05e12a257fa6fc53df56f3f705
Chris Griffing <cmgriffing@gmail.com>
2016-04-13 23:12:07 -0700

    Add regex commands for checking status of domain whois

--- api/src/domains.rs
+++ api/src/domains.rs
@@ -1,4 +1,5 @@
 use std::process::Command;
+use regex::Regex;
 
 pub fn whois(domain: String) -> String {
     //format!("whois lookup for {}", domain)
@@ -9,7 +10,20 @@ pub fn whois(domain: String) -> String {
                             .output().unwrap_or_else(|e| {
         panic!("failed to execute process: {}", e)
     });
-    String::from_utf8(whois.stdout).unwrap()
+    let whois_response = String::from_utf8(whois.stdout).unwrap();
+    
+    let whois_available_regex = Regex::new("No match for domain").unwrap();
+    let whois_unavailable_regex = Regex::new("Domain Name:").unwrap();    
+    
+    let mut whois_status = "error";
+    if whois_available_regex.is_match(&whois_response) {
+        whois_status = "available";
+    } else if whois_unavailable_regex.is_match(&whois_response) {
+        whois_status = "unavailable";
+    }
+    
+    return whois_status.to_string();
+    
 }
 
 // TODO: Consider adapting https://gist.github.com/gkbrk/0c2317e9f72dbe55695b
