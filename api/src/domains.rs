c4cf16164400eda8f0573b7c8cb7185fdea168f5
Chris Griffing <cmgriffing@gmail.com>
2016-04-13 23:35:37 -0700

    Fix formatting using rustfmt

--- api/src/domains.rs
+++ api/src/domains.rs
@@ -2,29 +2,28 @@ use std::process::Command;
 use regex::Regex;
 
 pub fn whois(domain: String) -> String {
-    
+
     let command_string = format!("whois {}", domain);
     let whois = Command::new("sh")
-        .arg("-c")
-        .arg(command_string)
-        .output().unwrap_or_else(|e| {
-            panic!("failed to execute process: {}", e)
-        });
-        
+                    .arg("-c")
+                    .arg(command_string)
+                    .output()
+                    .unwrap_or_else(|e| panic!("failed to execute process: {}", e));
+
     let whois_response = String::from_utf8(whois.stdout).unwrap();
-    
+
     let whois_available_regex = Regex::new("No match for domain").unwrap();
-    let whois_unavailable_regex = Regex::new("Domain Name:").unwrap();    
-    
+    let whois_unavailable_regex = Regex::new("Domain Name:").unwrap();
+
     let mut whois_status = "error";
     if whois_available_regex.is_match(&whois_response) {
         whois_status = "available";
     } else if whois_unavailable_regex.is_match(&whois_response) {
         whois_status = "unavailable";
     }
-    
+
     return whois_status.to_string();
-    
+
 }
 
 // TODO: Consider adapting https://gist.github.com/gkbrk/0c2317e9f72dbe55695b
--- api/src/synonyms.rs
+++ api/src/synonyms.rs
@@ -1,4 +1,7 @@
-use std::collections;
+// extern crate hyper;
+
+// use hyper::Client;
+// use hyper::header::Connection;
 
 pub fn lookup(word: String) -> String {
     format!("looking up synonyms for {}", word)
